e4c8fca96067e82aaebbcac436bf20de5bff89bd
Jayson1998XD <1104426533@qq.com>
2021-02-14 14:52:05 +0800

    进一步初始化，home模块创建

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <tab-bar>
+      <tab-bar-item path="/home" >
+        <img slot="item-icon" src="~assets/img/home/home.svg" alt="" srcset="">
+        <img slot="item-icon-active" src="~assets/img/home/home.active.svg" alt="" srcset="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" >
+        <img slot="item-icon" src="~assets/img/category/category.svg" alt="" srcset="">
+        <img slot="item-icon-active" src="~assets/img/category/category.active.svg" alt="" srcset="">
+        <div slot="item-text">标签</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" >
+        <img slot="item-icon" src="~assets/img/cart/cart.svg" alt="" srcset="">
+        <img slot="item-icon-active" src="~assets/img/cart/cart.active.svg" alt="" srcset="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" >
+        <img slot="item-icon" src="~assets/img/profile/profile.svg" alt="" srcset="">
+        <img slot="item-icon-active" src="~assets/img/profile/profile.active.svg" alt="" srcset="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+export default {
+  name: 'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
---
+++ src/views/category/Category.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>标签</h2>
+</template>
+
+<script>
+export default {
+  name: 'Category'
+}
+</script>

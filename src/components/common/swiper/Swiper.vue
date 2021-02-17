6c4a6de00ac2e540df2ff5369a721dcc4fddaffe
Jayson1998XD <1104426533@qq.com>
2021-02-17 14:41:43 +0800

    首页更新

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -54,6 +54,12 @@ export default {
   },
   mounted: function () {
     // 1.操作DOM, 在前后添加Slide
+    // setTimeout(() => {
+    //   this.handleDom();
+
+    //   // 2.开启定时器
+    //   this.startTimer();
+    // },100)
     this.$nextTick(function () {
       this.handleDom();
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends" />
+    <feature-view></feature-view>
   </div>
 </template>
 
@@ -12,12 +13,14 @@ import HomeSwiper from "./childComps/HomeSwiper"
 
 import { getHomeMultidata } from "network/home";
 import RecommendView from './childComps/RecommendView.vue';
+import FeatureView from './childComps/FeatureView'
 export default {
   name: "Home",
   components: {
     NavBar,
     HomeSwiper,
     RecommendView,
+    FeatureView
   },
   data() {
     return {
@@ -35,8 +38,18 @@ export default {
 </script>
 
 <style>
+#home{
+  padding-top: 44px;
+}
+
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
 </style>
